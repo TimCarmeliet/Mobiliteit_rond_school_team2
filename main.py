221f94e0cdc88731e4d72a998dfa75e5b5ddd76d
viggo-skippi <viggo.devries@takeerbergen.be>
2026-05-26 15:29:49 +0200

    tab_analyse vieuw gemaakt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from main_model import Model
 from main_controller import Controller
 from main_view import mainView
 from tab_logs import LogsTab
+from tab_analyse import AnalyseTab
 
 if __name__ == "__main__":
     root = tk.Tk()
